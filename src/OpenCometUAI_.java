18c7312abce7addd3b5e235a30b6c698e0140f3a
Gerardo Epitacio Galvez <ger.epitacio@gmail.com>
2015-11-16 20:22:46 -0600

    OpenComet to Netbeans form. OpenComet Dialog launch from ImageJ plugin: OpenCometUAI_

---
+++ src/OpenCometUAI_.java
@@ -0,0 +1,11 @@
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class OpenCometUAI_ implements PlugIn{
+    @Override
+    public void run(String string) {
+         IJ.log("Welcome to OpenComet");
+         OpenCometUAI.startJDialog();
+    }    
+}
